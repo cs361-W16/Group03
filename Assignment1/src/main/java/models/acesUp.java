08dc8d9ab5f45108486964f70294e56db517ed98
Pranav Ramesh <rameshp@oregonstate.edu>
2016-01-22 15:18:01 -0800

    initialized deck and player

--- Assignment1/src/main/java/models/acesUp.java
+++ Assignment1/src/main/java/models/acesUp.java
@@ -1,7 +1,16 @@
 package models;
+import models.Hand;
+import models.Player;
+import models.Deck;
+import models.Card;
+
 
 /**
- * Created by harderg on 1/19/16.
+ * Created by pranavramesh on 1/19/16.
  */
 public class acesUp {
+    Deck myDeck = new Deck();
+    Player player1 = new Player();
+
+
 }
