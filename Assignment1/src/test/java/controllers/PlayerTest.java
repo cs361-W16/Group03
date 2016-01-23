4ab65e4a1a66f4b006d51aeb8314e4e147f7d88a
mihaidan <danm@oregonstate.edu>
2016-01-22 17:57:14 -0800

    Deleted Repo and tried again
    Same spot I was at a bit ago, had to delete repo and try again.

--- Assignment1/src/test/java/controllers/PlayerTest.java
+++ Assignment1/src/test/java/controllers/PlayerTest.java
@@ -1,10 +1,30 @@
 package controllers;
 
+import models.Player;
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by Mihai on 1/21/2016.
  */
 public class PlayerTest {
+    @Test
+    public void testPlayerInput() {
+        Player playerTest = new Player();
+        Assert.assertEquals(playerTest.getPlayerInput(), 3);
+    }
 
+    @Test
+    public void testCurrValue() {
+        Player playerTest = new Player();
+        Assert.assertEquals(playerTest.getCurrValue(), 0);
+    }
+
+    @Test
+    public void testCurrSuit() {
+        Player playerTest = new Player();
+        Assert.assertEquals(playerTest.getCurrSuit(), '\u0000');
+    }
 }
