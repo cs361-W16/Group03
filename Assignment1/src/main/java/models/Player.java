e8fd6b5df05c21af2f26d4f2f750d63263fb0a09
hardergeorge <harderg@oregonstate.edu>
2016-02-03 10:12:49 -0800

    try and fix merge conflicts from end of assignment 1

--- Assignment1/src/main/java/models/Player.java
+++ Assignment1/src/main/java/models/Player.java
@@ -1,9 +1,9 @@
+
 package models;
 
 import models.Deck;
 import models.Hand;
-
-import java.util.Scanner;
+import models.Card;
 
 
 /**
@@ -11,42 +11,50 @@ import java.util.Scanner;
  */
 public class Player {
 
+    Hand newHand;
+
     public Player(){
-        Hand newHand = new Hand();
+        newHand = new Hand();
+    }
 
-        /*int cardValueStack1, cardValueStack2, cardValueStack3, cardValueStack4;
-        char cardSuitStack1, cardSuitStack2, cardSuitStack3, cardSuitStack4;
+    // Player input simulated for testing.
+    int playerInput = 1;
 
-        newHand.getTopCardValue(1) = cardValueStack1;
-        newHand.getTopCardValue(2) = cardValueStack2;
-        newHand.getTopCardValue(3) = cardValueStack3;
-        newHand.getTopCardValue(4) = cardValueStack4;
+    // Getting current card value and suit from Hand class.
+    int currCardValue = newHand.getTopCardValue(playerInput);
+    char currCardSuit = newHand.getTopCardSuit(playerInput);
 
-        newHand.getTopCardSuit(1) = cardSuitStack1;
-        newHand.getTopCardSuit(2) = cardSuitStack2;
-        newHand.getTopCardSuit(3) = cardSuitStack3;
-        newHand.getTopCardSuit(4) = cardSuitStack4;*/
-
-
-        Scanner inputReader = new Scanner(System.in);
-        System.out.println("Enter the column you wish to remove from: ");
-        int playerInput = inputReader.nextInt();
-
-        int currCardValue = newHand.getTopCardValue(playerInput);
-        char currCardSuit = newHand.getTopCardSuit(playerInput);
-
-        for(int i = 1; i < 5; i++){
-            if(currCardSuit == newHand.getTopCardSuit(i)){
-                if(currCardValue < newHand.getTopCardValue(i)){
-                    //Remove card function call here.
+    // Checks if the player made a legal move.
+    public void playerMove(){
+        for(int i = 1; i < 5; i++) {
+            // Checks if they are the same suit.
+            if (currCardSuit == newHand.getTopCardSuit(i)) {
+                // Checks if there is a card with a larger value than the one input.
+                if (currCardValue < newHand.getTopCardValue(i)) {
+                    // Removes the top card.
+                    newHand.popTopCard(playerInput);
                 }
             }
         }
+    }
 
+    // Return playerInput.
+    public int getPlayerInput(){
+        return playerInput;
+    }
 
+    // Return currCardValue.
+    public int getCurrValue(){
+        return currCardValue;
+    }
 
+    // Return currCardSuit.
+    public int getCurrSuit(){
+        return currCardSuit;
+    }
 
-
+    // Return newHand.
+    public Hand getNewHand(){
+        return newHand;
     }
 }
-
--- Assignment1/src/main/java/models/acesUp.java
+++ Assignment1/src/main/java/models/acesUp.java
@@ -1,7 +1,50 @@
 package models;
+import models.Hand;
+import models.Player;
+import models.Deck;
+import models.Card;
+
 
 /**
- * Created by harderg on 1/19/16.
+ * Created by pranavramesh on 1/19/16.
  */
 public class acesUp {
+    Deck myDeck;
+    Player player1;
+
+    public acesUp(){
+        myDeck = new Deck();
+        player1 = new Player();
+        myDeck.buildDeck();
+    }
+
+    public Deck getDeck(){
+        return myDeck;
+    }
+
+    /*public acesUp playGame(){
+        while(!winner()){
+            myDeck.dealCard();
+            player1.playerMove();
+            winner();
+            return player1;
+
+        }
+
+    }*/
+    // checks each stack to see if there is an Ace at the top.
+    // if there is then we declare the winner.
+    public boolean winner(){
+        if(player1.getNewHand().stack1[0].getValue() == 14){
+            if(player1.getNewHand().stack2[0].getValue() == 14){
+                if(player1.getNewHand().stack3[0].getValue() == 14){
+                    if(player1.getNewHand().stack4[0].getValue() == 14){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
 }
